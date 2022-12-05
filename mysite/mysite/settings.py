fd7eedd5b459089bb98aad6a3906aa8f71df71a3
vagrant <vagrant@vagrant.vm>
2022-12-05 20:11:36 +0000

    added LB5

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    # BASE_DIR / 'static'
 ]
 
 MEDIA_URL = '/media/'
